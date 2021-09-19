74528851eb9ce631419eaab09f0394c5d147c399
FernandaElola <elolafernanda@gmail.com>
2021-09-19 09:24:16 -0300

    agrego multer

--- src/routes/products.js
+++ src/routes/products.js
@@ -2,15 +2,32 @@
 const express = require('express');
 const router = express.Router();
 
+const path = require('path');
+const multer = require('multer');
+
 // ************ Controller Require ************
 const productsController = require('../controllers/productsController');
+
+/* configuración de multer */
+const storage = multer.diskStorage({
+    destination : (req,file,callaback) => {
+        callaback(null,'./public/images/products')
+    },
+    filename : (req,file,callaback) => {
+        callaback(null,'img-product-' + Date.now() + path.extname(file.originalname))
+    }
+});
+
+const upload = multer({
+    storage
+});
 
 /*** GET ALL PRODUCTS ***/ 
 router.get('/', productsController.index); 
 
 /*** CREATE ONE PRODUCT ***/ 
 router.get('/create', productsController.create); 
-router.post('/', productsController.store); 
+router.post('/', upload.single('image'), productsController.store); 
 
 
 /*** GET ONE PRODUCT ***/ 
@@ -23,7 +40,6 @@ router.put('/edit/:id', productsController.update);
 
 /*** DELETE ONE PRODUCT***/ 
 router.delete('/:id', productsController.destroy); 
-
 
 
 module.exports = router;
